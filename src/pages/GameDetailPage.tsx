acec9c9b4f86db7ee6982c76250ad2b1f6479c60
Jomesh <jomesh18@gmail.com>
2025-02-04 19:16:25 +0530

    routing finished 17

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,10 +1,10 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
-  // console.log(slug);
   const { isLoading, data: game, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
@@ -13,7 +13,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
